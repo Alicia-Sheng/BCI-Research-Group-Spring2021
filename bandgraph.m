62088fa83a612bcc82608c2c81fa271bb300e7d5
Alicia-Sheng <duowensheng@gmail.com>
2021-01-29 04:28:22 -0500

    added the sixth cluster to the overview graph

--- bandgraph.m
+++ bandgraph.m
@@ -81,8 +81,13 @@ axis([0 3000 -1 19]);
 %tcr_overview(data_matrix, 1 ,94);
 
 activity = [];
+activity = [data_matrix(1:600,:);data_matrix(3601:4200,:);data_matrix(6001:6600,:);data_matrix(9001:9600,:);data_matrix(12601:13200,:);data_matrix(15001:15600,:);
+    data_matrix(601:1200,:);data_matrix(4201:4800,:);data_matrix(8401:9000,:);data_matrix(9601:10200,:);data_matrix(12001:12600,:);data_matrix(15601:16200,:);
+    data_matrix(1201:1800,:);data_matrix(4801:5400,:);data_matrix(7201:7800,:);data_matrix(10201:10800,:);data_matrix(13201:13800,:);data_matrix(16801:17400,:);
+    data_matrix(1801:2400,:);data_matrix(3001:3600,:);data_matrix(6601:7200,:);data_matrix(10801:11400,:);data_matrix(14401:15000,:);data_matrix(16201:16800,:);
+    data_matrix(2401:3000,:);data_matrix(5401:6000,:);data_matrix(7801:8400,:);data_matrix(11401:12000,:);data_matrix(13801:14400,:);data_matrix(17401:18000,:)];
 %Think
-activity = [data_matrix(1:600,:);data_matrix(3601:4200,:);data_matrix(6001:6600,:);data_matrix(9001:9600,:);data_matrix(12601:13200,:);data_matrix(15001:15600,:)];
+%activity = [data_matrix(1:600,:);data_matrix(3601:4200,:);data_matrix(6001:6600,:);data_matrix(9001:9600,:);data_matrix(12601:13200,:);data_matrix(15001:15600,:)];
 %Count
 %activity = [data_matrix(601:1200,:);data_matrix(4201:4800,:);data_matrix(8401:9000,:);data_matrix(9601:10200,:);data_matrix(12001:12600,:);data_matrix(15601:16200,:)];
 %Recall
@@ -121,6 +126,8 @@ l4=plot([NaN, NaN], 'color', 'b');
 l5=plot([NaN, NaN], 'color', 'c');
 legend([l1,l2,l3,l4,l5],{'alpha','beta','delta','gamma','theta'});
 
-set(gca,'xtick',[600 1200 1800 2400 3000 3600]);
+%set(gca,'xtick',[0:600:3600]);
+set(gca,'xtick',[0:3600:18000]);
 
-axis([0 3600 -1 19]);
+%axis([0 3600 -1 21]);
+axis([0 18000 -1 21]);
